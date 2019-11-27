5ca822762b656d347358b8718519b59801cb5010
Sheldon <sheldonreiff@gmail.com>
2019-11-26 19:59:57 -0500

    fix urls

--- crawler.js
+++ crawler.js
@@ -6,42 +6,45 @@ const saveScreen = async (page, key = 'debug-screen') => {
 };
 
 const crawlPage = () => {
-    const products = [];
-    $('div[data-type="price-box"]').each( function() {
-        let note = null;
-        if($(this).find('div.add-to-cart-list-note:contains("Get It Free")').length > 0){
-            note = 'free';
-        }else if($(this).find('div.add-to-cart-list-note:contains("Available in a bundle with the Dugan Speech plugin")').length > 0){
-            note = 'dugan_bundle';
-        }else if($(this).find('div.add-to-cart-list-note:contains("Available only in selected bundles")').length > 0){
-            note = 'select_bundles';
+    const getNote = product => {
+        if(product.find('div.add-to-cart-list-note:contains("Get It Free")').length > 0){
+            return 'free';
+        }else if(product.find('div.add-to-cart-list-note:contains("Available in a bundle with the Dugan Speech plugin")').length > 0){
+            return 'dugan_bundle';
+        }else if(product.find('div.add-to-cart-list-note:contains("Available only in selected bundles")').length > 0){
+            return 'select_bundles';
         }
-        
-        let type = null;
-        if($(this).attr('data-url').indexOf('/plugins/') === 0){
-            type = 'plugin';
-        }else if($(this).attr('data-url').indexOf('/bundles/') === 0){
-            type = 'bundle';
+        return null;
+    }
+    const getTypeFromSlug = slug => {
+        const detectedType = slug.split('/')[1];
+        switch(detectedType){
+            case 'plugins':
+                return 'plugin';
+            case 'bundles':
+                return 'bundle';
         }
+        return null;
+    }
 
-        const slug = $(this).siblings('div.jplist-item-target').find('a').attr('href');
-
-        products.push({
+    const products = $('div[data-type="price-box"]').map( function() {
+        const slug = $(this).attr('data-url');
+        return {
             sku: $(this).attr('data-skunumber'),
             name: $(this).attr('data-name'),
-            type: type,
+            type: getTypeFromSlug(slug),
             msrp: parseFloat($(this).attr('data-msrp')),
             salePrice: parseFloat($(this).attr('data-skuprice')),
             category: $(this).attr('data-gsf-category'),
-            note: note,
+            note: getNote($(this)),
             saleEnd: $(this).attr('data-saledate'),
             badge: $(this).parent('article').attr('data-badge'),
             thumbnailUrl: $(this).siblings('div.jplist-item-target').find('div.icon img').attr('data-original'),
             url: `https://www.waves.com${slug}`,
-        });
+        }
     });
     console.log(`${products.length} products collected`);
-    return products;
+    return products.get();
 };
 
 const getData = async () => {
